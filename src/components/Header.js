0e4c12364657b7df6efa80840a1a00b36e28b7ae
Chori <slivers721@gmail.com>
2016-09-19 00:55:03 +0900

    fix #6
    ## Header Component 완료
    (Semantic UI Header)[http://semantic-ui.com/elements/header.html]
    (Header Component Source)[https://github.com/project-playground/play-ui/blob/0.2.0-SNAPSHOT/src/components/Header.js]

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,5 @@
 import React, { Component, PropTypes } from 'react';
 import UIComponent from './UIComponent';
-import header from 'semantic-ui-css/components/header.css';
 import classNames from 'classnames';
 
 const CLASSNAME = 'header';
@@ -12,43 +11,65 @@ export default class Header extends UIComponent {
   }
 
   getClassName() {
-    return classNames(super.getClassName(), CLASSNAME);
+    return classNames(super.getClassName(),
+        this.props.disabled == 'disabled' || this.props.disabled == 'true' ? 'disabled' : '',
+        this.props.align ? (this.props.align == 'justified' ? this.props.align : this.props.align + ' aligned') : '',
+        this.props.sub && (this.props.sub == 'sub' || this.props.sub == 'true') ? 'sub' : '',
+        this.props.divide && (this.props.divide == 'divide' || this.props.divide == 'true') ? 'dividing' : '',
+        this.props.block && (this.props.block == 'block' || this.props.block == 'true') ? 'block' : '',
+        this.props.attached ? this.props.attached : '',
+        this.props.floated ? this.props.floated : '',
+        this.props.color ? this.props.color : '',
+        this.props.inverted && (this.props.inverted == 'inverted' || this.props.inverted == 'true') ? 'inverted' : '',
+        CLASSNAME
+    );
   }
 
   render() {
-    var componentClass = this.getClassName();
-    if(this.props.align) {
-      componentClass = classNames(componentClass, this.props.align, 'aligned');
-    }
-    
+    let componentClass = this.getClassName();
     let content;
+    let subHeader;
     let iContent;
+    let imgContent;
     let textContent;
-    if(this.props.icon) {
-      let iconClass = classNames(this.props.icon, 'icon');
-      componentClass = classNames(componentClass, 'icon');
-      iContent = <i className={iconClass}></i>;
+    if(this.props.icon || this.props.contentIcon || this.props.img) {
+      if(this.props.icon || this.props.contentIcon) {
+        let iconClass;
+        if(this.props.icon) {
+          componentClass = classNames(componentClass, 'icon');
+        }
+
+        iconClass = classNames(this.props.icon || this.props.contentIcon, 'icon');
+
+        iContent = <i className={iconClass}></i>;
+      } else {
+        let imgClass = classNames('ui', this.props.img);
+        imgContent = <img className={imgClass} src={this.props.src}></img>;
+      }
+
       textContent = <div className="content">
                       {this.props.text}
-                      <div className="sub header">{this.props.subText}</div>
+                      <div className="sub header">{this.props.subHeader}</div>
                     </div>;
-      
     } else {
       content = this.props.text;
+      if(this.props.subHeader) {
+        subHeader = <div className="sub header">{this.props.subHeader}</div>
+      }
     }
 
     let header;
     
     if(this.props.type === 'h1') {
-      header = <h1 className={componentClass}>{content}{iContent}{textContent}</h1>;
+      header = <h1 className={componentClass}>{content}{subHeader}{iContent}{imgContent}{textContent}</h1>;
     } else if(this.props.type === 'h2') {
-      header = <h2 className={componentClass}>{content}{iContent}{textContent}</h2>;
+      header = <h2 className={componentClass}>{content}{subHeader}{iContent}{imgContent}{textContent}</h2>;
     } else if(this.props.type === 'h3') {
-      header = <h3 className={componentClass}>{content}{iContent}{textContent}</h3>;
+      header = <h3 className={componentClass}>{content}{subHeader}{iContent}{imgContent}{textContent}</h3>;
     } else if(this.props.type === 'h4') {
-      header = <h4 className={componentClass}>{content}{iContent}{textContent}</h4>;
+      header = <h4 className={componentClass}>{content}{subHeader}{iContent}{imgContent}{textContent}</h4>;
     } else if(this.props.type === 'h5') {
-      header = <h5 className={componentClass}>{content}{iContent}{textContent}</h5>;
+      header = <h5 className={componentClass}>{content}{subHeader}{iContent}{imgContent}{textContent}</h5>;
     }
 
     return header;
