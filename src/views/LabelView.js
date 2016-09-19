d472fa2c0652ae62784695dae23b60b4bb46a8d7
Byungil <bi_ahn@playground2016.cafe24.com>
2016-09-19 10:57:12 +0900

    Label component commit

--- src/views/LabelView.js
+++ src/views/LabelView.js
@@ -24,7 +24,36 @@ export default class LabelView extends React.Component {
 				<Label size="massive" text="Massive" />
 				
 				<h3>Icon</h3>
-				<Label icon="search" text="Search" />	
+				<Label icon="search" text="Search" />
+				<Label icon="delete" iconPos="right" text="Delete" />
+				
+				<h3>Tag</h3>
+				<Label tag="true" text="New" />
+				<Label tag="true" colored="red" text="Uncoming" />
+				<Label tag="true" colored="teal" text="Featured" />
+				
+				<h3>Circular</h3>
+				<Label circular="true" colored="red" text="1" />
+				<Label circular="true" colored="orange" text="2" />
+				<Label circular="true" colored="yellow" text="3" />
+				<Label circular="true" colored="green" text="4" />
+				<Label circular="true" colored="teal" text="5" />
+				
+				<h3>Colored</h3>
+				<Label colored="red" text="Red" />
+				<Label colored="orange" text="Orange" />
+				<Label colored="yellow" text="Yellow" />
+				<Label colored="olive" text="Olive" />
+				<Label colored="green" text="Green" />
+				<Label colored="teal" text="Teal" />
+				<Label colored="blue" text="Blue" />
+				<Label colored="violet" text="Violet" />
+				<Label colored="purple" text="Purple" />
+				<Label colored="pink" text="Pink" />
+				<Label colored="brown" text="Brown" />
+				<Label colored="grey" text="Grey" />
+				<Label colored="black" text="Black" />
+
 				
 				<Divider />
 			</div>		
