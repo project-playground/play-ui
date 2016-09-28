06ac118718d770be2f43e79cc4cb1ab13e97fdac
chori <chori@playground2016.cafe24.com>
2016-09-28 17:07:11 +0900

    fix #15 ## Step Component added

---
+++ src/components/Steps.js
@@ -0,0 +1,46 @@
+import React, { Component, PropTypes } from 'react';
+import UIComponent from './UIComponent';
+import classNames from 'classnames';
+import Step from './Step'
+
+const CLASSNAME = 'steps';
+
+export default class Steps extends UIComponent {
+  constructor(props) {
+    super(props);
+  }
+
+  getClassName() {
+    return classNames(super.getClassName(),
+                      this.props.size ? this.props.size : '',
+                      this.props.divide ? this.props.divide : '',
+                      this.props.fluid == 'fluid' || this.props.fluid ? 'fluid' : '',
+                      this.props.attached ? this.props.attached + ' attached' : '',
+                      this.props.type,
+                      CLASSNAME);
+  }
+
+  render() {
+    let componentClass = this.getClassName();
+    let steps = this.props.steps.map((step) => {
+      return <Step key={step.id}
+                   icon={step.icon}
+                   title={step.title}
+                   description={step.description}
+                   active={step.active}
+                   disabled={step.disabled}
+                   completed={step.completed}
+                   link={step.link} />
+    });
+    
+    return (
+      <div className={componentClass}>
+        {steps}
+      </div>
+    );
+  };
+}
+
+Steps.propTypes = {
+  steps: PropTypes.array.isRequired
+}
