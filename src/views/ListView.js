74dcccad0b5b74e23b61b1b35b652a239844459e
Gyunsu <gyunsu@playground2016.cafe24.com>
2016-10-17 21:24:39 +0900

    fix #27 listview

---
+++ src/views/ListView.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import LinkItems from '../collections/LinkItems';
+import Item from '../collections/Item';
+
+import List from '../components/List';
+
+export default class ListView extends React.Component {
+	render() {
+		return (
+			<div>
+
+			</div>
+		);
+	}
+}
