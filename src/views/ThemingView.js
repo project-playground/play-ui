199795d96dea0ede5ebf3a004026c92ba7a6abfe
jjin0117 <densang002@naver.com>
2016-10-23 01:27:58 +0900

    fix #40 Theming - site

--- src/views/ThemingView.js
+++ src/views/ThemingView.js
@@ -19,9 +19,15 @@ export default class ThemingView extends React.Component {
 						<Header size={3} text="Heading 3" />
 						<Header size={4} text="Heading 4" />
 						<Header size={5} text="Heading 5" />
+						<p>Nullam quis risus eget urna mollis ornare vel eu leo. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam id dolor id nibh ultricies vehicula.</p>
 					</Column>
 					<Column>
-						<Header size={2} text="Example body text" />
+						 <Header size={2} text="Heading 2" />
+						 <p>Nullam quis risus eget <a href="#">urna mollis ornare</a> vel eu leo. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam id dolor id nibh ultricies vehicula.</p>
+						 <p><small>This line of text is meant to be treated as fine print.</small></p>
+						 <p>The following snippet of text is <strong>rendered as bold text</strong>.</p>
+						 <p>The following snippet of text is <em>rendered as italicized text</em>.</p>
+						 <p>An abbreviation of the word attribute is <abbr title="attribute">attr</abbr>.</p>				
 					</Column>
 					<Column>
 						<Grid column={3} stackable={true} padded={true} valign="middle" centered={true} aligned={true}>
@@ -29,11 +35,19 @@ export default class ThemingView extends React.Component {
 							<Column colored="orange">Orange</Column>
 							<Column colored="yellow">Yellow</Column>
 							<Column colored="olive">Olive</Column>
+							<Column colored="green">Green</Column>
+							<Column colored="teal">Teal</Column>
 							<Column colored="blue">Blue</Column>
 							<Column colored="violet">Violet</Column>
+							<Column colored="purple">Purple</Column>
+							<Column colored="pink">Pink</Column>
+							<Column colored="brown">Brown</Column>
+							<Column colored="grey">Grey</Column>
+							<Column colored="black">Black</Column>
 						</Grid>
 					</Column>
 				</Grid>
+				<Header size={2} divide={true} text="Menu" />
 			</div>		
 		);
 	}
