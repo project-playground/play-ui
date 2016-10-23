db3281a712ad7e071df13340ed92afb41538e9c7
jjin0117 <densang002@naver.com>
2016-10-24 00:49:19 +0900

    fix #40 dropdownitem

--- src/views/ThemingView.js
+++ src/views/ThemingView.js
@@ -8,7 +8,7 @@ import Menu from '../collections/Menu';
 import Item from '../collections/Item';
 import LinkItem from '../collections/LinkItem';
 import Dropdown from '../modules/Dropdown';
-import DropdownItem from '../modules/DropdownItem';
+import DropdownItem from '../collections/DropdownItem';
 import Divider from '../components/Divider';
 import Input from '../components/Input';
 import Button from '../components/Button';
